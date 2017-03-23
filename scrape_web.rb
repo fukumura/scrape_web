b2d01ff6d04c3e15ca55bb6d8af7d21b88e408d3
akinori.fukumura <fukumura.akinori@gmail.com>
2017-03-23 23:23:58 +0900

    ruby でscraping.かんたん

---
+++ scrape_web.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'nokogiri'
+
+url = ARGV[0]
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+ end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+p doc.title
